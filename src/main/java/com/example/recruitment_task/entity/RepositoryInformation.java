7ec02558fc735dd50caad4b14f508f23003a4939
Weronika Graczyk <wercia01@onet.pl>
2023-08-31 18:36:16 +0200

    create RepositoryInformation entity

---
+++ src/main/java/com/example/recruitment_task/entity/RepositoryInformation.java
@@ -0,0 +1,8 @@
+package com.example.recruitment_task.entity;
+
+import java.util.List;
+
+public class RepositoryInformation {
+    private String repositoryName;
+    private List<BranchInformation> branches;
+}
