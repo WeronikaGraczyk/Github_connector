296ed663c5c137f9a08ccaa7f5ffd44a77b63b6a
Weronika Graczyk <wercia01@onet.pl>
2023-08-31 19:01:17 +0200

    create UserGithubInformation entity

---
+++ src/main/java/com/example/recruitment_task/entity/UserGithubInformation.java
@@ -0,0 +1,8 @@
+package com.example.recruitment_task.entity;
+
+import java.util.List;
+
+public class UserGithubInformation {
+    private String userName;
+    private List<RepositoryInformation> repositories;
+}
