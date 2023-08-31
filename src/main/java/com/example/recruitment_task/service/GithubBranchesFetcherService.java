ea8a2ec51eff6946a67fd43f6387fd553412d8b2
Weronika Graczyk <wercia01@onet.pl>
2023-08-31 20:06:54 +0200

    create GithubBranchesFetcherService

---
+++ src/main/java/com/example/recruitment_task/service/GithubBranchesFetcherService.java
@@ -0,0 +1,18 @@
+package com.example.recruitment_task.service;
+
+import com.example.recruitment_task.entity.BranchInformation;
+import com.example.recruitment_task.entity.RepositoryInformation;
+import com.example.recruitment_task.entity.UserGithubInformation;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//@Service
+@AllArgsConstructor
+public class GithubBranchesFetcherService {
+
+}
