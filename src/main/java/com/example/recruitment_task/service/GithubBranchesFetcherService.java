ec0d4a3bebe8d416b89f8c63d100d37d7a55ead9
Weronika Graczyk <wercia01@onet.pl>
2023-09-01 16:13:24 +0200

    update GithubBranchesFetcherService to working version

--- src/main/java/com/example/recruitment_task/service/GithubBranchesFetcherService.java
+++ src/main/java/com/example/recruitment_task/service/GithubBranchesFetcherService.java
@@ -3,13 +3,15 @@ package com.example.recruitment_task.service;
 import com.example.recruitment_task.entity.BranchInformation;
 import com.example.recruitment_task.entity.RepositoryInformation;
 import com.example.recruitment_task.entity.UserGithubInformation;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,39 +21,39 @@ import java.util.Objects;
 
 @Service
 @NoArgsConstructor
+@AllArgsConstructor
+@Component
 public class GithubBranchesFetcherService {
     private static final String GITHUB_API_URL = "https://api.github.com/";
     private RestTemplate restTemplate = new RestTemplate();
-    @Value("${github}")
-    private String authToken;
 
+    @Value("${github.access.token}")
+    private String token;
 
     public UserGithubInformation fetchUserGithubInformation(String userName) {
-        System.out.println(authToken);
         HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(authToken);
+        headers.setBearerAuth(token);
+
         ResponseEntity<RepositoryInformation[]> response = restTemplate.exchange(
                 GITHUB_API_URL + "/users/{userName}/repos",
                 HttpMethod.GET,
                 new HttpEntity<>(headers),
                 RepositoryInformation[].class, userName);
         RepositoryInformation[] repositories = response.getBody();
+
         List<RepositoryInformation> withoutForkRespositories = new ArrayList<>();
         for (RepositoryInformation repoInfo : repositories) {
             if (!repoInfo.isFork()) {
-                repoInfo.setBranches(getBranchesListFromRepository(repoInfo.getName(), userName, authToken));
+                repoInfo.setBranches(getBranchesListFromRepository(repoInfo.getName(), userName));
                 withoutForkRespositories.add(repoInfo);
             }
         }
-        System.out.println(withoutForkRespositories.size());
-//        Arrays.stream(new List[]{withoutForkRespositories})
-//                .forEach(repoInfo -> System.out.println(repoInfo));
         return new UserGithubInformation(userName, withoutForkRespositories);
     }
 
-    private List<BranchInformation> getBranchesListFromRepository(String repositoryName, String userName, String authToken) {
+    private List<BranchInformation> getBranchesListFromRepository(String repositoryName, String userName) {
         HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(authToken);
+        headers.setBearerAuth(token);
 
         ResponseEntity<BranchInformation[]> response = restTemplate.exchange(
                 GITHUB_API_URL + "/repos/{userName}/{repositoryName}/branches",
@@ -71,6 +73,7 @@ public class GithubBranchesFetcherService {
             branchInformationsResult.add(new BranchInformation(branchInfo.getName(),
                     Objects.requireNonNull(responseCommitSha.getBody()).getSha()));
         }
+
         return branchInformationsResult;
     }
 }
